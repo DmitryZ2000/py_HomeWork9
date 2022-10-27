cbc121970194772758fd33d9b13d401e2c61ffc4
Dmitry Zaykin <108060580+DmitryZ2000@users.noreply.github.com>
2022-10-27 16:30:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from bots_commands import *
 
-app = ApplicationBuilder().token("5609000734:AAE0popPteNjQAo_NOxp2r_UyIiTHzV7q-A").build()
+app = ApplicationBuilder().token("Your token").build()
 
 app.add_handler(CommandHandler("hi", hi_command))
 app.add_handler(CommandHandler("game", game_command))
